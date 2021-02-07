0dd30e86d569fe33748c9026e3de6dedf97f02c2
namitduggal <namitduggal9@gmail.com>
2021-02-08 01:04:54 +0530

    url

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://agile-stream-44368.herokuapp.com/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
