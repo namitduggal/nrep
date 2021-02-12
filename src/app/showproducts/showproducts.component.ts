a20513ab135a8ab93a117692d0e1fedae19648b2
namitduggal <namitduggal9@gmail.com>
2021-02-12 20:01:45 +0530

    on create

--- src/app/showproducts/showproducts.component.ts
+++ src/app/showproducts/showproducts.component.ts
@@ -4,19 +4,7 @@ import { HttpClient } from '@angular/common/http';
 
 
 import { Directive, Output, EventEmitter, Input, SimpleChange} from '@angular/core';
-@Directive({
-  selector: '[onCreate]'
-})
 
-export class OnCreate {
-
-  @Output() onCreate: EventEmitter<any> = new EventEmitter<any>();
-  constructor() {}
-  ngOnInit() {      
-     this.onCreate.emit('dummy'); 
-  } 
-
-}
 
 @Component({
   selector: 'app-showproducts',
