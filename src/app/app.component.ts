b01a2900925fa7ffe37b78254e74d0cda486ba16
namitduggal <namitduggal9@gmail.com>
2021-02-12 20:53:56 +0530

    usertype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eCommerceProj';
+  constructor(private route:ActivatedRoute,private router:Router,private myhttp:HttpClient) { 
+    router.events.subscribe(event=>{
+      if(event instanceof NavigationEnd){
+        this.ngOnInit();
+      }
+      })
+    }
+      ngOnInit(){
+  var usertype="normal";
+  usertype=sessionStorage.getItem("utype");
+    }
 }
+
