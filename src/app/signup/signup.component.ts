135df8eb7ef926b3e07df934022e2f5221a35628
namitduggal <namitduggal9@gmail.com>
2021-02-14 00:22:29 +0530

    correction in signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -29,7 +29,7 @@ export class SignupComponent implements OnInit {
     if(myform.valid)
     {
     var myparams={nm:this.nam,uname:this.un,pass:this.passw};
-    this.myhttp.post("https://ecommerce-webpage-try.herokuapp.com/api/signupapi/signup",myparams,{responseType:"text"}).subscribe((res)=>
+    this.myhttp.post("https://ecommerce-webpage-try.herokuapp.com/api/signup",myparams,{responseType:"text"}).subscribe((res)=>
     {
       this.msg=res;
     });
