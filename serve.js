640bcc70d6137e74db9e59958f75a89ec1a7abb3
namitduggal <namitduggal9@gmail.com>
2021-02-17 18:26:04 +0530

    cors final changes

--- serve.js
+++ serve.js
@@ -38,13 +38,13 @@ let upload = multer({storage: storage});
 
 
 //for cors
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
  res.setHeader('Access-Control-Allow-Origin', '*');
  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
  res.setHeader('Access-Control-Allow-Credentials', true);
  next();
-});
+});*/
 app.use(cors());
 var mongoose = require("mongoose");
 const MongoClient = require('mongodb').MongoClient;
